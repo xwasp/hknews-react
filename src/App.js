f170d3e87f5a3200e85d9f396775a6093b8e96c6
Prafull <prafullgn@gmail.com>
2021-03-21 17:39:40 +0530

    added list item

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React from 'react'
 
-const welcome = {
-  greeting : 'Hey',
-  title    : 'React'
-}
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectId: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectId: 1,
+  },
+];
 
-function getTitle(title){
-  return title;
-}
+
 
 function App(){
   //do something in between
-
   return(
     <div>
-      <h1>{welcome.greeting} {welcome.title}</h1>
-      <h1>Hello {getTitle('React')}</h1>
+      <h1>My Hacker Stories</h1>
 
       <label htmlFor="search" type="text">Search: </label>
       <input id="search" type="text"/>
+
+      <hr/>
+
+      {list.map(function(item){
+        return <div key={item.objectId}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+          </div>
+      })}
     </div>
   );
 }
