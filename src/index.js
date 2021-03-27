62d074716f1fc1cdb2e69d9959212a8e6e832e62
Prafull <prafullgn@gmail.com>
2021-03-27 14:10:28 +0530

    removed tick

--- src/index.js
+++ src/index.js
@@ -10,19 +10,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-  
 
-function tick(){
-  const element = (
-  <div>
-    <h1>Hello world!</h1>
-    <h2>It is {new Date().toLocaleTimeString()}.</h2>
-  </div>
-  );
-  ReactDOM.render(element,document.getElementById('root'));
-}
-
-setInterval(tick,1000);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
